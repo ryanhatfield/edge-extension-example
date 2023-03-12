953a9200a9f38cbf8c00f5b38dc1fe6751fd5d9a
Ryan Hatfield <rhatfield@bushelpowered.com>
2023-03-12 00:04:57 -0600

    Add back working stuff

--- background.js
+++ background.js
@@ -1,86 +1,23 @@
-// This is a `Cloud Device Description`, more information here:
-// https://developers.google.com/cloud-print/docs/cdd#cdd
-const CDD = {
-  version: '1.0',
-  printer: {
-    supported_content_type: [
-      { content_type: 'application/pdf', min_version: '1.5' },
-      { content_type: 'image/jpeg' },
-      { content_type: 'text/plain' },
-    ],
-    input_tray_unit: [
-      {
-        vendor_id: 'tray',
-        type: 'INPUT_TRAY',
-      },
-    ],
-    marker: [
-      {
-        vendor_id: 'black',
-        type: 'INK',
-        color: { type: 'BLACK' },
-      },
-      {
-        vendor_id: 'color',
-        type: 'INK',
-        color: { type: 'COLOR' },
-      },
-    ],
-    cover: [
-      {
-        vendor_id: 'front',
-        type: 'CUSTOM',
-        custom_display_name: 'front cover',
-      },
-    ],
-    vendor_capability: [],
-    color: {
-      option: [
-        { type: 'STANDARD_MONOCHROME' },
-        { type: 'STANDARD_COLOR', is_default: true },
-        {
-          vendor_id: 'ultra-color',
-          type: 'CUSTOM_COLOR',
-          custom_display_name: 'Best Color',
-        },
-      ],
-    },
-    copies: {
-      default: 1,
-      max: 100,
-    },
-    media_size: {
-      option: [
-        {
-          name: 'ISO_A4',
-          width_microns: 210000,
-          height_microns: 297000,
-          is_default: true,
-        },
-        {
-          name: 'NA_LEGAL',
-          width_microns: 215900,
-          height_microns: 355600,
-        },
-        {
-          name: 'NA_LETTER',
-          width_microns: 215900,
-          height_microns: 279400,
-        },
-      ],
-    },
-  },
+importScripts('./cdd.js')
+
+function sendBytesSomewhere(pdf) {
+  console.log('sendBytesSomewhere')
+  console.log('pdf:', pdf)
+
+  pdf.arrayBuffer().then((arrayBuffer) => {
+    console.log(arrayBuffer)
+  })
 }
 
-function sendWithFtps(pdf, callback) {
-  console.log('sendWithFtps')
-  console.log('pdf:', pdf)
+function openInNewWindow(pdf) {
+  var blobURL = URL.createObjectURL(pdf)
+  window.open(blobURL)
 }
 
 // The onPrintRequested event is fired after the print dialog has been closed
 function onPrintRequested(printJob, resultCallback) {
   console.log('onPrintRequested')
-  sendWithFtps(printJob)
+  sendBytesSomewhere(printJob.document)
   resultCallback('OK')
 }
 
@@ -104,7 +41,10 @@ function onGetPrintersRequested(resultCallback) {
 function onGetCapabilityRequested(printerId, resultCallback) {
   console.log('onGetCapabilityRequested')
   console.log(printerId)
+  // const { href } = new URL('cdd.js', location.href)
+  // __shimport__.load(href).then((CDD) => {
   resultCallback(CDD)
+  // })
 }
 
 // This adds a lambda function, and can be used for initialization
